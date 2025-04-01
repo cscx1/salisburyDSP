819f7f971a711855cb90f339830206b714c1506e
Aidan Levy <alevy4@hpcl2-6.hslinux>
2025-03-31 20:36:03 -0400

    Updated HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       </h1>
 
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        For the Spring 2025 semester, Aidan and Diego are undertaking an independent study research course under the guidance of Dr. Cone, focusing on applications of Digital Signal Processing and Linear Algebra...
+      Our research project implements audio filters in Python without relying on external DSP (Digital Signal Processing) libraries, reinforcing a deep understanding of signal manipulation. We visualize frequency domain transformations in DSP through custom JavaScript and D3 programming code.  Our research group also has developed a web-based interface into these analytic tools wherein users can input a YouTube link, apply audio effects such as bass boost, chorus, and reverb, and then download the processed audio. Discrete time graphs are generated to help visualize how each DSP effect transforms the waveform. This tool not only demonstrates core DSP concepts but also serves as a foundation for accessible, real time processing platforms.
       </p>
 
       <div className="flex justify-center my-10">
